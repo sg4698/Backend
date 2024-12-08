97d093c0d516415b750462a11b336324fb8328f8
Shivam Gupta <guptashivam8005@gmail.com>
2024-12-08 19:10:01 +0530

    changes in user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,7 @@
 const  asyncHandler = require("../utils/asyncHandler")
 const userRouter = require("../routes/user.routes")
 
-const userRegister = asyncHandler(async () => {
+const userRegister = asyncHandler(async (req,res) => {
     // Sends a response with a status code of 200 (OK) and a JSON object containing the message "ok".
     res.status(200).json({
         message: "ok"
